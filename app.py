ae8edc7ec7d4dc0494f0330b218293d2bac8746c
bdauria1 <103322615+bdauria1@users.noreply.github.com>
2023-06-04 22:06:35 -0400

    started working on the backend of the website

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template
+import mysql.connector
+
+connection = mysql.connector.connect(
+    host='localhost', 
+    database='unstaffed', 
+    user='root', 
+    password='root')
+
+cursor = connection.cursor()
+
+app = Flask(__name__, static_url_path='/static')
+app.debug = True
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('Home.html')
+
+@app.route("/Company_login", methods=['GET', 'POST'])
+def clogin():
+    if flask.request.method == 'GET':
+        return render_template('Company_login.html')
+    else:
+        email = Flask.request.form['email']
+        password = Flask.request.form['password']
+
+        # Check if the email and password exist in the User table
+        query = "SELECT * FROM User WHERE role = 'company' AND email = %s AND password = %s"
+        values = (email, password)
+        cursor.execute(query, values)
+        result = cursor.fetchone()  # Fetch a single row
+
+        if result:
+            # Login successful
+            # You can perform additional actions here, such as setting session variables
+            return render_template('Company_home.html')
+        else:
+            # Login failed
+            return render_template('Company_login.html', error='Invalid email or password')
+        
+
+
+@app.route("/Freelancer_login", methods=['GET'])
+def flogin():
+    return render_template('Freelancer_login.html')
+
+@app.route("/Signup", methods=['GET'])
+def signup():
+    return render_template('Signup.html')
+
--- database.sql
+++ database.sql
@@ -1,28 +1,57 @@
-CREATE TABLE users (
-  id INTEGER PRIMARY KEY,
-  username TEXT UNIQUE NOT NULL,
-  password TEXT NOT NULL,
-  email TEXT NOT NULL,
-  is_company BOOLEAN NOT NULL
+-- Drop the existing tables (if they exist) to start fresh
+DROP TABLE IF EXISTS Chat;
+DROP TABLE IF EXISTS Message;
+DROP TABLE IF EXISTS Email;
+DROP TABLE IF EXISTS User;
+
+-- Create the User table
+CREATE TABLE User (
+  user_id INT PRIMARY KEY AUTO_INCREMENT,
+  role ENUM('freelancer', 'company') NOT NULL,
+  profile_pic VARCHAR(255),
+  about_me TEXT,
+  field VARCHAR(255),
+  location VARCHAR(255),
+  language VARCHAR(255),
+  education VARCHAR(255),
+  projects VARCHAR(255),
+  pricing VARCHAR(255),
+  name VARCHAR(255),
+  phone VARCHAR(255),
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE services (
-  id INTEGER PRIMARY KEY,
-  user_id INTEGER NOT NULL,
-  title TEXT NOT NULL,
-  description TEXT NOT NULL,
-  price DECIMAL(10,2) NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (user_id) REFERENCES users(id)
+-- Create the Chat table
+CREATE TABLE Chat (
+  chat_id INT PRIMARY KEY AUTO_INCREMENT,
+  freelancer_id INT,
+  company_id INT,
+  FOREIGN KEY (freelancer_id) REFERENCES User(user_id),
+  FOREIGN KEY (company_id) REFERENCES User(user_id)
 );
 
-CREATE TABLE orders (
-  id INTEGER PRIMARY KEY,
-  buyer_id INTEGER NOT NULL,
-  service_id INTEGER NOT NULL,
-  status TEXT NOT NULL,
-  price DECIMAL(10,2) NOT NULL,
-  created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  FOREIGN KEY (buyer_id) REFERENCES users(id),
-  FOREIGN KEY (service_id) REFERENCES services(id)
+-- Create the Message table
+CREATE TABLE Message (
+  message_id INT PRIMARY KEY AUTO_INCREMENT,
+  chat_id INT,
+  sender_id INT,
+  receiver_id INT,
+  message TEXT,
+  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (chat_id) REFERENCES Chat(chat_id),
+  FOREIGN KEY (sender_id) REFERENCES User(user_id),
+  FOREIGN KEY (receiver_id) REFERENCES User(user_id)
+);
+
+-- Create the Email table
+CREATE TABLE Email (
+  email_id INT PRIMARY KEY AUTO_INCREMENT,
+  sender_id INT,
+  receiver_id INT,
+  subject VARCHAR(255),
+  message TEXT,
+  timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (sender_id) REFERENCES User(user_id),
+  FOREIGN KEY (receiver_id) REFERENCES User(user_id)
 );
