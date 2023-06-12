0cd1d72f489fb136c0c9e9de88ed555c7cbbe9f8
bdauria1 <103322615+bdauria1@users.noreply.github.com>
2023-06-11 22:56:21 -0400

    configured backend

--- app.py
+++ app.py
@@ -1,70 +1,28 @@
 from flask import Flask, render_template
-import mysql.connector
-
-connection = mysql.connector.connect(
-    host='localhost', 
-    database='unstaffed', 
-    user='root', 
-    password='root')
-
-cursor = connection.cursor()
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path='/static')
 app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
 
-@app.route('/', methods=['GET'])
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.Text, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    def __repr__(self):
+        return '<Name %r>' % self.id
+
+@app.route('/')
 def home():
     return render_template('Home.html')
 
-@app.route("/Company_login", methods=['GET', 'POST'])
+@app.route("/Company_login")
 def clogin():
-    if flask.request.method == 'GET':
-        return render_template('Company_login.html')
-    else:
-        email = Flask.request.form['email']
-        password = Flask.request.form['password']
+    return render_template('Company_login.html')
 
-        # Check if the email and password exist in the User table
-        query = "SELECT * FROM User WHERE role = 'company' AND email = %s AND password = %s"
-        values = (email, password)
-        cursor.execute(query, values)
-        result = cursor.fetchone()  # Fetch a single row
-
-        if result:
-            # Login successful
-            # You can perform additional actions here, such as setting session variables
-            return render_template('Company_home.html')
-        else:
-            # Login failed
-            return render_template('Company_login.html', error='Invalid email or password')
-        
-
-@app.route("/Freelancer_login", methods=['GET', 'POST'])
-def flogin():
-    if flask.request.method == 'GET':
-        return render_template('Freelancer_login.html')
-    else:
-        email = flask.request.form['email']
-        password = flask.request.form['password']
-
-        # Create a cursor to execute SQL queries
-        cursor = connection.cursor()
-
-        # Check if the email and password exist in the User table
-        query = "SELECT * FROM User WHERE role = 'freelancer' AND email = %s AND password = %s"
-        values = (email, password)
-        cursor.execute(query, values)
-        result = cursor.fetchone()  # Fetch a single row
-
-        if result:
-            # Login successful
-            # You can perform additional actions here, such as setting session variables
-            return "Login successful"
-        else:
-            # Login failed
-            return "Invalid email or password"
-
-@app.route("/Signup", methods=['GET'])
-def signup():
-    return render_template('Signup.html')
-
+# Create the database tables
+with app.app_context():
+    db.create_all()
